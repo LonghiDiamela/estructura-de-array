632a04411e46e08645548ffb10ba38da40ba079e
LonghiDiamela <110548653+LonghiDiamela@users.noreply.github.com>
2022-09-19 17:37:56 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+const num:number []=[20, 14, 8, 0, 5, 19, 24];
+console.log(num);
+
+for (let i=0; i<num.length; i++) {
+  console.log(`en indice: ${i}. Elemento: ${num[i]}.`);
+  
+}
